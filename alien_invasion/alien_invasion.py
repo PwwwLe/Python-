68f480eb2a812b8229e282a3a66e77da50b7e6fa
Sliver_0812 <116070589+PwwwLe@users.noreply.github.com>
2023-06-20 15:23:24 +0800

    添加了飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,25 +4,71 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
 
-def run_game():
-    # 初始化游戏，创建一个屏幕对象
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width,ai_settings.screen_height)
-    )
-    pygame.display.set_caption("Alien Invasion")
 
-    # 开始游戏主循环
-    while True:
+class AlienInvasion:
+    def __init__(self):
+        # 初始化游戏，创建一个屏幕对象
+        pygame.init()
+
+        # 控制帧率
+        self.clock = pygame.time.Clock()
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        # 开始游戏主循环
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+            # 设置游戏帧率
+            self.clock.tick(60)
+
+    def _check_events(self):
         # 监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(ai_settings.bg_color)
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按下"""
+        if event.key == pygame.K_d:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应释放"""
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        # 每次循环重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
         # 让最近绘制的屏幕可见
         pygame.display.update()
 
 
-run_game()
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,9 +1,12 @@
-class Settings():
+class Settings:
     """用于存储所有设置的类"""
+
     def __init__(self):
         """初始化游戏设置"""
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
 
+        # 飞船设置
+        self.ship_speed = 1.5
